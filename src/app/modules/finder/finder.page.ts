92c37c646229f1d6b895716062f55a2b78698db2
Danel <dceresa1988@gmail.com>
2022-04-19 16:31:56 +0200

    ✨ feat(Finder): Search box
    Search box with rxjs pipes to control data input and backpack filtering. Backpacks are filtered based on the search term

--- src/app/modules/finder/finder.page.ts
+++ src/app/modules/finder/finder.page.ts
@@ -1,5 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { IonInput } from '@ionic/angular';
+import { AsyncPipe } from '@angular/common';
+import {
+  Component,
+  ElementRef,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { BehaviorSubject, fromEvent, Subject, Subscription } from 'rxjs';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  filter,
+  map,
+} from 'rxjs/operators';
 import { BackpackService } from 'src/app/core/services/backpack.service';
 import { Backpack } from 'src/app/shared/model/backpack';
 
@@ -7,10 +21,45 @@ import { Backpack } from 'src/app/shared/model/backpack';
   selector: 'app-finder',
   templateUrl: './finder.page.html',
   styleUrls: ['./finder.page.scss'],
+  providers: [AsyncPipe],
 })
-export class FinderPage implements OnInit {
-  backpacks$: Observable<Backpack[]> = this.backpackService.getBackpacks();
-  constructor(public backpackService: BackpackService) {}
+export class FinderPage implements OnInit, OnDestroy {
+  @ViewChild('searchTerm') input: IonInput;
+  allBackpacks: Backpack[];
+  backpacks$: BehaviorSubject<Backpack[]> = new BehaviorSubject([]);
+  subs: Subscription;
+  constructor(
+    public backpackService: BackpackService,
+    public asyncPipe: AsyncPipe
+  ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.backpackService.getBackpacks().subscribe((backpacks) => {
+      this.allBackpacks = backpacks;
+      this.backpacks$.next(backpacks);
+    });
+  }
+  ionViewDidEnter() {
+    this.subs = this.input.ionInput
+      .pipe(
+        map((e: any) => e.target.value),
+        debounceTime(150),
+        distinctUntilChanged()
+      )
+      .subscribe((term: string) => {
+        this.filterBackpacks(term);
+      });
+  }
+  filterBackpacks(term) {
+    const filteredBackpacks = this.allBackpacks.filter((backpack) =>
+      backpack.name
+        ? backpack.name.toLowerCase().indexOf(term.toLowerCase()) > -1
+        : false
+    );
+    this.backpacks$.next(filteredBackpacks);
+  }
+
+  ngOnDestroy() {
+    this.subs.unsubscribe();
+  }
 }
